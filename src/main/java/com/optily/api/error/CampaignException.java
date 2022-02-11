cca3d3dd1acb317d702ab46d6cf0a413bfe6ce5b
satyadeep singh <satyadeep.singh@avrioc.com>
2022-02-11 17:31:59 +0400

    show specific error message

--- src/main/java/com/optily/api/error/CampaignException.java
+++ src/main/java/com/optily/api/error/CampaignException.java
@@ -1,12 +1,13 @@
 package com.optily.api.error;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @AllArgsConstructor
-@ToString
 @Data
 @NoArgsConstructor
 public class CampaignException extends RuntimeException {
@@ -14,5 +15,13 @@ public class CampaignException extends RuntimeException {
     private int code;
     private String description;
 
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Error: [");
+        sb.append("code=").append(code);
+        sb.append(", description='").append(description).append('\'');
+        sb.append("]");
+        return sb.toString();
+    }
 
 }
